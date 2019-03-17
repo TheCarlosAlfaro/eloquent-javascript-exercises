21db6a526e2fefb7fa62276d5c571de36e797bce
devbysalas <rgkalin@gmail.com>
2019-03-16 21:58:03 -0700

    First exercise

---
+++ chapter-2/loopingATriangle.js
@@ -0,0 +1,9 @@
+let poundSign = "";
+for (let i = 1; i < 8; i++) {
+  if (poundSign.length < i) {
+    poundSign += "#";
+    console.log(poundSign);
+  } else {
+    console.log(poundSign);
+  }
+}
