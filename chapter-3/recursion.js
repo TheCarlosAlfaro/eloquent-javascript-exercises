9994d9eb9323c4fc88cbe19e0deadb49eeeb8b95
devbysalas <rgkalin@gmail.com>
2019-03-20 12:09:54 -0700

    Recursion

---
+++ chapter-3/recursion.js
@@ -0,0 +1,10 @@
+const isEven = num => {
+  if (num < 0) {
+    return false;
+  }
+  if (num == 0) return true;
+  else if (num == 1) return false;
+  else return isEven(num - 2);
+};
+
+console.log(isEven(50));
